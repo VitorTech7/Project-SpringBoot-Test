983d14c89c6dd9a93410a3c144f9ab62773ebf61
Vitor <vytroxalberto@gmail.com>
2023-07-06 20:56:43 -0300

    First Step in Java with Spring Boot 3

---
+++ src/main/java/br/com/springboottest/ProjectSpringBootTest/Startup.java
@@ -0,0 +1,13 @@
+package br.com.springboottest.ProjectSpringBootTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Startup {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Startup.class, args);
+	}
+
+}
